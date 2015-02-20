7b20381623c441b6a3b28e60d9ec6877483206bb
Bruce Alderson <bruce@napkinware.com>
2015-02-19 16:53:42 -0800

    Adds URL parser.

--- lib/Service.php
+++ lib/Service.php
@@ -36,12 +36,42 @@ class Service {
 
 	}
 
-	/**/
-	public function __call($fn, $args) {
-
-	}
 
 	private function request($resource, $method = 'get', $resourceType = 'json') {
 
+	}
+
+	static function decompose($resource, $explicitType = null) {
+		$defaults = array(
+			'scheme' => 'http',
+			'host' => 'localhost',
+			'path' => '',
+			'query' => (object) array()
+		);
+
+		$url = parse_url($resource);
+		$url = (object) array_merge($defaults, $url);
+
+		if (!empty($url->query) && is_string($url->query)) {
+			parse_str($url->query, $q);
+			$url->query = (object) $q;
+		}
+
+		$parts = preg_split('/\//', $url->path, -1, PREG_SPLIT_NO_EMPTY);
+
+		$type = $explicitType;
+		if ( ( $at = strrpos($url->path, '.') ) ) {
+			$type = substr($url->path, $at + 1);
+		}
+
+		$url->path = (object) array(
+			'raw' => $url->path,
+			'parts' => $parts,
+			'type' => $type,
+			'explicitType' => $explicitType,
+			'vars' => preg_grep("/\:[^\/\.]/", $parts)
+		);
+		
+		return (object) $url;
 	}
 }
--- test/ServiceTests.php
+++ test/ServiceTests.php
@@ -1,3 +1,30 @@
 <?php
 
 /* Tests for Service library */
+
+include_once('lib/Service.php');
+
+use NapkinWare\PrestoPHP\Service;
+
+class ServiceTests extends PHPUnit_Framework_TestCase {
+    public function testURIDecomposition() {
+
+    $parts = Service::decompose('/orgs/:org/repos');
+    print_r($parts);
+    $parts = Service::decompose('//api.github.com/orgs/:org/:repos');
+    print_r($parts);
+    $parts = Service::decompose('https://api.github.com/repositories');
+    print_r($parts);
+    $parts = Service::decompose('https://api.github.com/repositories', 'application/json');
+    print_r($parts);
+    $parts = Service::decompose('https://api.github.com/orgs/:org/repos');
+    print_r($parts);
+    $parts = Service::decompose('https://api.github.com/orgs/:org/repos');
+    print_r($parts);
+    $parts = Service::decompose('https://api.github.com/orgs/:org/repos.json?page=2&limit=5');
+    print_r($parts);
+
+//        $this->assertEquals(-1, $b->getAmount());
+
+    }
+}
